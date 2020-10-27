952682fc2252ea2ffc8cb38b8fd5e68481cc3ed3
Pablo Marino <pmarino2013@gmail.com>
2020-10-27 14:53:16 -0300

    Agregando comentarios al código

--- src/component/Paginator.jsx
+++ src/component/Paginator.jsx
@@ -2,47 +2,47 @@ import React, { useState, useEffect } from "react";
 import ReactPaginate from "react-paginate";
 
 export default function Paginator() {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0); //manejo el número de página
+  const [data, setData] = useState([]); //guardo la data
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  //funcion para setear la página seleccionada
   function handlePageClick({ selected }) {
     setCurrentPage(selected);
   }
 
+  //GET a api donde está la info a mostrar
   function fetchData() {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((data) => {
-        // console.log(data);
-        //   const {
-        //     course: { uploads },
-        //   } = data;
         setData(data);
       });
   }
+
   const PER_PAGE = 10; //cantidad de elementos por página
   const offset = currentPage * PER_PAGE; // número de elementos que ya se han mostrado en las páginas anteriores.
   const currentPageData = data
     .slice(offset, offset + PER_PAGE) //devuelve una copia de una parte del array dentro de un nuevo array empezando por inicio hasta fin
-    .map(({ body }) => <p>{body} </p>);
+    .map(({ body }) => <p>{body} </p>); //map del nuevo array
 
+  //variable que contiene la cantidad de páginas que tendra
   const pageCount = Math.ceil(data.length / PER_PAGE);
 
   return (
     <div className="container">
       <h1>React Paginate Example</h1>
-      <nav aria-label="Page navigation example">
+      <nav>
         <ReactPaginate
           previousLabel={"← Previous"}
           nextLabel={"Next →"}
           breakLabel={"..."}
-          breakClassName={"page-link"}
-          pageCount={pageCount}
-          onPageChange={handlePageClick}
+          breakClassName={"page-link"} //estilo de los ...
+          pageCount={pageCount} //cantidad de páginas
+          onPageChange={handlePageClick} //cuando hago clic en una página
           // pageClassName={"page-link"}
           pageLinkClassName={"page-link"}
           containerClassName={"pagination"}
