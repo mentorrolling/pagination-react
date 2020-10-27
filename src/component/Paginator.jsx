164425c902cea050ab7fdebaf6c863bc65f65b60
Pablo Marino <pmarino2013@gmail.com>
2020-10-27 14:32:55 -0300

    Ejemplo de paginacion con React-paginate

---
+++ src/component/Paginator.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
+
+export default function Paginator() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function handlePageClick({ selected }) {
+    setCurrentPage(selected);
+  }
+
+  function fetchData() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        //   const {
+        //     course: { uploads },
+        //   } = data;
+        setData(data);
+      });
+  }
+  const PER_PAGE = 10; //cantidad de elementos por página
+  const offset = currentPage * PER_PAGE; // número de elementos que ya se han mostrado en las páginas anteriores.
+  const currentPageData = data
+    .slice(offset, offset + PER_PAGE) //devuelve una copia de una parte del array dentro de un nuevo array empezando por inicio hasta fin
+    .map(({ body }) => <p>{body} </p>);
+
+  const pageCount = Math.ceil(data.length / PER_PAGE);
+
+  return (
+    <div className="container">
+      <h1>React Paginate Example</h1>
+      <nav aria-label="Page navigation example">
+        <ReactPaginate
+          previousLabel={"← Previous"}
+          nextLabel={"Next →"}
+          breakLabel={"..."}
+          breakClassName={"page-link"}
+          pageCount={pageCount}
+          onPageChange={handlePageClick}
+          // pageClassName={"page-link"}
+          pageLinkClassName={"page-link"}
+          containerClassName={"pagination"}
+          previousLinkClassName={"page-link"}
+          nextLinkClassName={"page-link"}
+          disabledClassName={"page-item disabled"}
+          activeClassName={"page-item active"}
+        />
+      </nav>
+
+      {currentPageData}
+    </div>
+  );
+}
